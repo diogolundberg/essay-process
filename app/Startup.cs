8f31885f7f6abe0dacf55770b91ba0b6dd86cf75
Diogo Lundberg <dclundberg@gmail.com>
2018-07-02 00:04:18 -0300

    Add S3 config and SDK

--- app/Startup.cs
+++ app/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.Runtime;
+using Amazon.S3;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -24,6 +26,10 @@ namespace app
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+      var awsOptions = Configuration.GetAWSOptions();
+      awsOptions.Credentials = new EnvironmentVariablesAWSCredentials();
+      services.AddDefaultAWSOptions(awsOptions);
+      services.AddAWSService<IAmazonS3>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
