29336ce7096fbe3ff5215dc5dc38d88120bdbbf8
Diogo Lundberg <dclundberg@gmail.com>
2018-07-01 23:06:57 -0300

    Add AWS SDK

---
+++ app/Controllers/MessagesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace app.Controllers
+{
+  [Route("/")]
+  public class MessagesController : Controller
+  {
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return new OkObjectResult("ready");
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody]dynamic value)
+    {
+      var info = value.Records.First.s3;
+
+      return new OkObjectResult(new
+      {
+        bucket = info.bucket.name,
+        key = info["object"].key,
+      });
+    }
+  }
+}
